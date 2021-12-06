6a2db66a7e9ad2a4b30df130e0c70c87b18ac824
Dionisius2k17 <34659280+Dionisius2k17@users.noreply.github.com>
2021-12-06 22:57:15 +0300

    Task 8: correction in TestAsFront.java file

--- TaskThree_Litecart/src/test/java/ru/stqa/maven/TestAsAdministrator.java
+++ TaskThree_Litecart/src/test/java/ru/stqa/maven/TestAsAdministrator.java
@@ -10,7 +10,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class TestAsAdministrator extends TestBasis{
-
     @Before
     public void openHomePage(){
         driver.get("http://localhost/litecart/admin/");
@@ -33,64 +32,6 @@ public class TestAsAdministrator extends TestBasis{
                 ss.get(n).click();
                 assertTrue(isElementPresent(By.tagName("h1")));
             }
-        }
-    }
-
-    @Test
-    public void testTwo(){
-        driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
-        List<WebElement> countries= driver.findElements(By.cssSelector("tr.row a:not([title])"));
-        List<String> countryNames = new ArrayList<>();
-        for (int i=0; i<countries.size(); i++){
-            String s = countries.get(i).getText();
-            countryNames.add(s);
-        }
-        List<String> defaultCountries = countryNames;
-        Collections.sort(countryNames);
-        assert countryNames.equals(defaultCountries);
-        List<WebElement> getRows= driver.findElements(By.cssSelector("tr.row"));
-        for (int i=0; i<getRows.size();i++){
-            List<WebElement> rows= driver.findElements(By.cssSelector("tr.row"));
-            WebElement row = rows.get(i);
-            WebElement zoneVal=row.findElement(By.cssSelector("td:nth-of-type(6)"));
-            String s=zoneVal.getText();
-            int zoneNum = Integer.parseInt(s);
-            if (zoneNum > 0){
-                WebElement country=row.findElement(By.cssSelector("a:not([title])"));
-                country.click();
-                List<WebElement> zones= driver.findElements(By.cssSelector("table#table-zones tr:not(.header) input[type=hidden][value][name*=name]"));
-                List<String> zoneNames=new ArrayList<>();
-                for (int n=0;n<zones.size();n++){
-                    String z=zones.get(n).getText();
-                    zoneNames.add(z);
-                }
-                List<String> zoneDefault=zoneNames;
-                Collections.sort(zoneNames);
-                assert zoneNames.equals(zoneDefault);
-                driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
-            } else {
-                continue;
-            }
-        }
-    }
-
-    @Test
-    public void testThree(){
-        driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
-        List<WebElement> countries=driver.findElements(By.cssSelector("tr.row a:not([title])"));
-        for (int i=0; i<countries.size(); i++){
-            List<WebElement> countryNames=driver.findElements(By.cssSelector("tr.row a:not([title])"));
-            countryNames.get(i).click();
-            List<WebElement> zones=driver.findElements(By.cssSelector("select[name*=zone_code]"));
-            List<String> zoneList=new ArrayList<>();
-            for (int n=0; n<zones.size(); n++){
-                String zoneName=zones.get(n).getText();
-                zoneList.add(zoneName);
-            }
-            List<String> zoneDefault=zoneList;
-            Collections.sort(zoneList);
-            assert zoneDefault.equals(zoneList);
-            driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
         }
     }
 }
--- TaskThree_Litecart/src/test/java/ru/stqa/maven/TestAsFront.java
+++ TaskThree_Litecart/src/test/java/ru/stqa/maven/TestAsFront.java
@@ -1,24 +1,33 @@
 package ru.stqa.maven;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TestAsFront extends TestBasis{
+
+    @Before
+    public void start(){
+        driver.get("http://localhost/litecart/");
+    }
+
+
     /*
     В функции ниже находим список всех изображений, создаётся цикл для их перебора.
     Для каждого из таких изображений необходимо будет получить список стикеров, после чего проверяем их количество.
-
     */
     @Test
     public void testOne(){
-        driver.get("http://localhost/litecart/");
         List<WebElement> images = driver.findElements(By.cssSelector("li.product"));
-        for (int i=0; i<images.size();i++){
-            List<WebElement> sticker= images.get(i).findElements(By.cssSelector("div.image-wrapper>div[class*=sticker]"));
-            assert sticker.size()==1;
+        for (int i = 0; i<images.size(); i++){
+            List<WebElement> sticker= images.get(i).findElements(By.cssSelector("div.image-wrapper>div.sticker"));
+            assert sticker.size() == 1;
         }
     }
 }
